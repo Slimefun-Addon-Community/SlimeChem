b327e27bf367f3486c226b21ae58b9af82d5f636
Seggan <segganew@gmail.com>
2020-12-01 15:58:01 -0500

    Fixed interaction

--- src/main/java/io/github/mooy1/slimechem/implementation/atomic/IngredientItem.java
+++ src/main/java/io/github/mooy1/slimechem/implementation/atomic/IngredientItem.java
@@ -1,12 +1,20 @@
 package io.github.mooy1.slimechem.implementation.atomic;
 
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
+import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
 import io.github.thebusybiscuit.slimefun4.core.attributes.NotPlaceable;
+import io.github.thebusybiscuit.slimefun4.core.attributes.ProtectionType;
+import io.github.thebusybiscuit.slimefun4.core.handlers.EntityInteractHandler;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
 import lombok.Getter;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -27,10 +35,11 @@ public class IngredientItem extends SlimefunItem implements NotPlaceable {
 
     @Getter
     private Ingredient ingredient;
-    private static final Map<Ingredient, Consumer<Player>> interactActions = getActions();
     private static final Set<Ingredient> radioactiveItems = new HashSet<>();
+    private static final Map<Ingredient, Consumer<Player>> interactActions;
 
     static {
+        Bukkit.getLogger().info("Run0");
         // Add all elements with atomic number > 82 (higher than lead)
         for (Element element : Element.values()) {
             if (element.getNumber() > 82) {
@@ -46,6 +55,9 @@ public class IngredientItem extends SlimefunItem implements NotPlaceable {
                 radioactiveItems.add(isotope);
             }
         }
+
+
+        interactActions = getActions();
     }
 
     public IngredientItem(Category category, Ingredient ingredient, RecipeType recipeType, ItemStack[] recipe) {
@@ -53,13 +65,33 @@ public class IngredientItem extends SlimefunItem implements NotPlaceable {
 
         this.ingredient = ingredient;
         addItemHandler(onUse());
+        addItemHandler(onEntityUse());
+    }
+
+    private EntityInteractHandler onEntityUse() {
+        return (e, i, b) -> {
+            e.setCancelled(true);
+            Bukkit.getLogger().info("Run1");
+            Player p = e.getPlayer();
+            Consumer<Player> run = interactActions.get(ingredient);
+            if (run != null) {
+                Bukkit.getLogger().info("Run2");
+                PlayerInventory inv = p.getInventory();
+                inv.setItemInMainHand(consumeItem(inv.getItemInMainHand()));
+                run.accept(p);
+            }
+        };
     }
 
     private ItemUseHandler onUse() {
         return e -> {
+            e.cancel();
+            Bukkit.getLogger().info("Run1");
             Player p = e.getPlayer();
             Consumer<Player> run = interactActions.get(ingredient);
+            Bukkit.getLogger().info(Integer.toString(interactActions.size()));
             if (run != null) {
+                Bukkit.getLogger().info("Run2");
                 PlayerInventory inv = p.getInventory();
                 inv.setItemInMainHand(consumeItem(inv.getItemInMainHand()));
                 run.accept(p);
@@ -70,7 +102,29 @@ public class IngredientItem extends SlimefunItem implements NotPlaceable {
     private static Map<Ingredient, Consumer<Player>> getActions() {
         Map<Ingredient, Consumer<Player>> actions = new HashMap<>();
 
-        // TODO: add actions
+        for (Ingredient ingredient : radioactiveItems) {
+
+            Bukkit.getLogger().info("Run4");
+            actions.put(ingredient, p -> {
+                for (Entity e : p.getNearbyEntities(5, 5, 5)) {
+                    if (e instanceof LivingEntity) {
+                        Bukkit.getLogger().info("Run3");
+                        if (e instanceof Player) {
+                            Player player = (Player) e;
+                            Optional<PlayerProfile> optionalPlayerProfile = PlayerProfile.find(player);
+                            if (optionalPlayerProfile.isPresent()) {
+                                if (optionalPlayerProfile.get().hasFullProtectionAgainst(ProtectionType.RADIATION)) {
+                                    continue;
+                                }
+                            } else {
+                                PlayerProfile.request(player);
+                            }
+                        }
+                        e.setFireTicks(200);
+                    }
+                }
+            });
+        }
 
         return actions;
     }
