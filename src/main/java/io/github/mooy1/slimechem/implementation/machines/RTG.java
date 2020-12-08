86c0476e7374daa055fd69e16d3dfdc713756a50
Seggan <segganew@gmail.com>
2020-12-08 09:08:59 -0500

    Started RTG

--- src/main/java/io/github/mooy1/slimechem/implementation/machines/RTG.java
+++ src/main/java/io/github/mooy1/slimechem/implementation/machines/RTG.java
@@ -1,67 +1,295 @@
 package io.github.mooy1.slimechem.implementation.machines;
 
-import io.github.mooy1.slimechem.lists.Categories;
-import io.github.mooy1.slimechem.lists.Items;
-import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetProvider;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemState;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
+import io.github.thebusybiscuit.slimefun4.implementation.items.electric.AbstractEnergyProvider;
+import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
+import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
+import io.github.thebusybiscuit.slimefun4.utils.itemstack.ItemStackWrapper;
 import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu.AdvancedMenuClickHandler;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ClickAction;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AGenerator;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.MachineFuel;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
+import me.mrCookieSlime.Slimefun.api.item_transport.ItemTransportFlow;
+import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
+import me.mrCookieSlime.Slimefun.cscorelib2.protection.ProtectableAction;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.HashMap;
+import java.util.Map;
 
-public class RTG extends SlimefunItem implements EnergyNetProvider {
-    
-    public static final int URANIUM_ENERGY = 6;
-    public static final int PLUTONIUM_ENERGY = 36;
-    public static final int AMERICIUM_ENERGY = 120;
-    public static final int CALIFORNIUM_ENERGY = 360;
-    
-    private final Type type;
-    
-    public RTG(Type type) {
-        super(Categories.MACHINES, type.getItem(), RecipeType.ENHANCED_CRAFTING_TABLE, type.getRecipe());
-        this.type = type;
+/**
+ * This generator, based off of {@link AGenerator}, consumes items and gives byproducts from them
+ *
+ * @author Seggan
+ * @author TheBusyBiscuit (original AGenerator author)
+ *
+ * @see AGenerator
+ */
+public abstract class RTG extends AbstractEnergyProvider {
+
+    public static Map<Location, MachineFuel> processing = new HashMap<>();
+    public static Map<Location, Integer> progress = new HashMap<>();
+
+    private static final int[] border = {0, 1, 2, 3, 4, 5, 6, 7, 8, 13, 31, 36, 37, 38, 39, 40, 41, 42, 43, 44};
+    private static final int[] border_in = {9, 10, 11, 12, 18, 21, 27, 28, 29, 30};
+    private static final int[] border_out = {5, 6, 7, 8, 14, 23, 32, 41, 42, 43, 44};
+
+    private int energyProducedPerTick = -1;
+    private int energyCapacity = -1;
+
+    @ParametersAreNonnullByDefault
+    public RTG(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(category, item, recipeType, recipe);
+
+        new BlockMenuPreset(item.getItemId(), getInventoryTitle()) {
+
+            @Override
+            public void init() {
+                constructMenu(this);
+            }
+
+            @Override
+            public boolean canOpen(Block b, Player p) {
+                return p.hasPermission("slimefun.inventory.bypass") || SlimefunPlugin.getProtectionManager().hasPermission(p, b.getLocation(), ProtectableAction.INTERACT_BLOCK);
+            }
+
+            @Override
+            public int[] getSlotsAccessedByItemTransport(ItemTransportFlow flow) {
+                if (flow == ItemTransportFlow.INSERT) {
+                    return getInputSlots();
+                } else {
+                    return getOutputSlots();
+                }
+            }
+        };
+
+        registerBlockHandler(item.getItemId(), (p, b, tool, reason) -> {
+            BlockMenu inv = BlockStorage.getInventory(b);
+
+            if (inv != null) {
+                inv.dropItems(b.getLocation(), getInputSlots());
+                inv.dropItems(b.getLocation(), getOutputSlots());
+            }
+
+            progress.remove(b.getLocation());
+            processing.remove(b.getLocation());
+            return true;
+        });
+
+        registerDefaultFuelTypes();
     }
-    
-    @Override
-    public int getGeneratedOutput(@Nonnull Location l, @Nonnull Config data) {
-        return type.getEnergy();
+
+    private void constructMenu(BlockMenuPreset preset) {
+        for (int i : border) {
+            preset.addItem(i, ChestMenuUtils.getBackground(), ChestMenuUtils.getEmptyClickHandler());
+        }
+
+        for (int i : border_in) {
+            preset.addItem(i, ChestMenuUtils.getInputSlotTexture(), ChestMenuUtils.getEmptyClickHandler());
+        }
+
+        for (int i : border_out) {
+            preset.addItem(i, ChestMenuUtils.getOutputSlotTexture(), ChestMenuUtils.getEmptyClickHandler());
+        }
+
+        for (int i : getOutputSlots()) {
+            preset.addMenuClickHandler(i, new AdvancedMenuClickHandler() {
+
+                @Override
+                public boolean onClick(Player p, int slot, ItemStack cursor, ClickAction action) {
+                    return false;
+                }
+
+                @Override
+                public boolean onClick(InventoryClickEvent e, Player p, int slot, ItemStack cursor, ClickAction action) {
+                    return cursor == null || cursor.getType() == null || cursor.getType() == Material.AIR;
+                }
+            });
+        }
+
+        preset.addItem(22, new CustomItem(Material.BLACK_STAINED_GLASS_PANE, " "), ChestMenuUtils.getEmptyClickHandler());
     }
 
     @Override
+    public int[] getInputSlots() {
+        return new int[]{19, 20};
+    }
+
+    @Override
+    public int[] getOutputSlots() {
+        return new int[]{15, 16, 17, 24, 25, 26, 33, 34, 35};
+    }
+
+    public MachineFuel getProcessing(Location l) {
+        return processing.get(l);
+    }
+
+    public boolean isProcessing(Location l) {
+        return progress.containsKey(l);
+    }
+
+    @Override
+    public int getGeneratedOutput(Location l, Config data) {
+        BlockMenu inv = BlockStorage.getInventory(l);
+
+        if (isProcessing(l)) {
+            int timeleft = progress.get(l);
+
+            if (timeleft > 0) {
+                ChestMenuUtils.updateProgressbar(inv, 22, timeleft, processing.get(l).getTicks(), getProgressBar());
+
+                if (isChargeable()) {
+                    int charge = getCharge(l, data);
+
+                    if (getCapacity() - charge >= getEnergyProduction()) {
+                        progress.put(l, timeleft - 1);
+                        return getEnergyProduction();
+                    }
+
+                    return 0;
+                } else {
+                    progress.put(l, timeleft - 1);
+                    return getEnergyProduction();
+                }
+            } else {
+                ItemStack fuel = processing.get(l).getInput();
+
+                if (isBucket(fuel)) {
+                    inv.pushItem(new ItemStack(Material.BUCKET), getOutputSlots());
+                }
+
+                inv.replaceExistingItem(22, new CustomItem(Material.BLACK_STAINED_GLASS_PANE, " "));
+
+                progress.remove(l);
+                processing.remove(l);
+                return 0;
+            }
+        } else {
+            Map<Integer, Integer> found = new HashMap<>();
+            MachineFuel fuel = findRecipe(inv, found);
+
+            if (fuel != null) {
+                for (Map.Entry<Integer, Integer> entry : found.entrySet()) {
+                    inv.consumeItem(entry.getKey(), entry.getValue());
+                }
+
+                processing.put(l, fuel);
+                progress.put(l, fuel.getTicks());
+            }
+
+            return 0;
+        }
+    }
+
+    private boolean isBucket(@Nullable ItemStack item) {
+        if (item == null) {
+            return false;
+        }
+
+        ItemStackWrapper wrapper = new ItemStackWrapper(item);
+        return item.getType() == Material.LAVA_BUCKET || SlimefunUtils.isItemSimilar(wrapper, SlimefunItems.FUEL_BUCKET, true) || SlimefunUtils.isItemSimilar(wrapper, SlimefunItems.OIL_BUCKET, true);
+    }
+
+    private MachineFuel findRecipe(BlockMenu menu, Map<Integer, Integer> found) {
+        for (MachineFuel fuel : fuelTypes) {
+            for (int slot : getInputSlots()) {
+                if (fuel.test(menu.getItemInSlot(slot))) {
+                    found.put(slot, fuel.getInput().getAmount());
+                    return fuel;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * This method returns the max amount of electricity this machine can hold.
+     *
+     * @return The max amount of electricity this Block can store.
+     */
     public int getCapacity() {
-        return type.getEnergy() * 8;
+        return energyCapacity;
     }
-    
-    @Getter
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    public enum Type {
-        
-        URANIUM(URANIUM_ENERGY, Items.URANIUM_RTG, new ItemStack[] {
-                
-        }),
-        PLUTONIUM(PLUTONIUM_ENERGY, Items.PLUTONIUM_RTG, new ItemStack[] {
 
-        }),
-        AMERICIUM(AMERICIUM_ENERGY, Items.AMERICIUM_RTG, new ItemStack[] {
-
-        }),
-        CALIFORNIUM(CALIFORNIUM_ENERGY, Items.CALIFORNIUM_RTG, new ItemStack[] {
-
-        }),
-        ;
-        
-        private final int energy;
-        @Nonnull
-        private final SlimefunItemStack item;
-        @Nonnull
-        private final ItemStack[] recipe;
+    /**
+     * This method returns the amount of energy that is consumed per operation.
+     *
+     * @return The rate of energy consumption
+     */
+    @Override
+    public int getEnergyProduction() {
+        return energyProducedPerTick;
     }
-    
+
+    /**
+     * This sets the energy capacity for this machine.
+     * This method <strong>must</strong> be called before registering the item
+     * and only before registering.
+     *
+     * @param capacity The amount of energy this machine can store
+     * @return This method will return the current instance of {@link me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AGenerator}, so that can be chained.
+     */
+    public final RTG setCapacity(int capacity) {
+        Validate.isTrue(capacity >= 0, "The capacity cannot be negative!");
+
+        if (getState() == ItemState.UNREGISTERED) {
+            this.energyCapacity = capacity;
+            return this;
+        } else {
+            throw new IllegalStateException("You cannot modify the capacity after the Item was registered.");
+        }
+    }
+
+    /**
+     * This method sets the energy produced by this machine per tick.
+     *
+     * @param energyProduced The energy produced per tick
+     * @return This method will return the current instance of {@link me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AGenerator}, so that can be chained.
+     */
+    public final RTG setEnergyProduction(int energyProduced) {
+        Validate.isTrue(energyProduced > 0, "The energy production must be greater than zero!");
+
+        this.energyProducedPerTick = energyProduced;
+        return this;
+    }
+
+    @Override
+    public void register(@Nonnull SlimefunAddon addon) {
+        this.addon = addon;
+
+        if (getCapacity() < 0) {
+            warn("The capacity has not been configured correctly. The Item was disabled.");
+            warn("Make sure to call '" + getClass().getSimpleName() + "#setEnergyCapacity(...)' before registering!");
+        }
+
+        if (getEnergyProduction() <= 0) {
+            warn("The energy consumption has not been configured correctly. The Item was disabled.");
+            warn("Make sure to call '" + getClass().getSimpleName() + "#setEnergyProduction(...)' before registering!");
+        }
+
+        if (getCapacity() >= 0 && getEnergyProduction() > 0) {
+            super.register(addon);
+        }
+    }
+
 }
--- src/main/java/io/github/mooy1/slimechem/lists/Items.java
+++ src/main/java/io/github/mooy1/slimechem/lists/Items.java
@@ -27,39 +27,6 @@ public final class Items {
             "",
             LoreUtils.energy(ChemicalCombiner.ENERGY) + "per item"
     );
-    
-    public static final SlimefunItemStack URANIUM_RTG = new SlimefunItemStack(
-            "URANIUM_RTG",
-            Material.POLISHED_BLACKSTONE,
-            "&aUranium RTG",
-            "&7Constantly generates power from the decay of uranium-238",
-            "",
-            LoreUtils.energyPerSecond(RTG.URANIUM_ENERGY)
-    );
-    public static final SlimefunItemStack PLUTONIUM_RTG = new SlimefunItemStack(
-            "PLUTONIUM_RTG",
-            Material.POLISHED_BLACKSTONE,
-            "&aUranium RTG",
-            "&7Constantly generates power from the decay of plutonium-238",
-            "",
-            LoreUtils.energyPerSecond(RTG.PLUTONIUM_ENERGY)
-    );
-    public static final SlimefunItemStack AMERICIUM_RTG = new SlimefunItemStack(
-            "AMERICIUM_RTG",
-            Material.POLISHED_BLACKSTONE,
-            "&aUranium RTG",
-            "&7Constantly generates power from the decay of americium-241",
-            "",
-            LoreUtils.energyPerSecond(RTG.AMERICIUM_ENERGY)
-    );
-    public static final SlimefunItemStack CALIFORNIUM_RTG = new SlimefunItemStack(
-            "CALIFORNIUM_RTG",
-            Material.POLISHED_BLACKSTONE,
-            "&aUranium RTG",
-            "&7Constantly generates power from the decay of californium-250",
-            "",
-            LoreUtils.energyPerSecond(RTG.CALIFORNIUM_ENERGY)
-    );
 
     public static final SlimefunItemStack ADDON_INFO = new SlimefunItemStack(
             SlimeChem.getInstance().getName().toUpperCase(Locale.ROOT) + "_ADDON_INFO",
