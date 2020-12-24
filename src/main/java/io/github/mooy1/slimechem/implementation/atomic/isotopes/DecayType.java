9eda0dc143f399da7356601aebf266fab7fd9322
Seggan <segganew@gmail.com>
2020-12-24 12:49:55 -0500

    Refactor DecayType

--- src/main/java/io/github/mooy1/slimechem/implementation/atomic/isotopes/DecayType.java
+++ src/main/java/io/github/mooy1/slimechem/implementation/atomic/isotopes/DecayType.java
@@ -3,13 +3,16 @@ package io.github.mooy1.slimechem.implementation.atomic.isotopes;
 import lombok.AllArgsConstructor;
 
 import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
 
 @AllArgsConstructor
 public enum DecayType {
+
     ALPHA("alpha"),
     BETA_PLUS("beta+"),
     BETA_MINUS("beta-"),
-    DOUBLE_BETA_MINUS("beta-beta-"),
+    DOUBLE_BETA_MINUS("double beta-"),
     PROTON("p"),
     PROTON_2("2p"),
     PROTON_3("3p"),
@@ -17,20 +20,26 @@ public enum DecayType {
     NEUTRON_2("2n"),
     NEUTRON_3("3n"),
     NEUTRON_4("4n"),
-    ELECTRON_CAPTURE("capture"),
-    DOUBLE_ELECTRON_CAPTURE("Double"),
+    ELECTRON_CAPTURE("electron capture"),
+    DOUBLE_ELECTRON_CAPTURE("double electron capture"),
     STABLE("stable");
 
+    private static final Map<String, DecayType> reprmap = new HashMap<>();
     private final String representation;
 
     @Nonnull
     public static DecayType getByRepresentation(String representation) {
-        for (DecayType decayType : DecayType.values()) {
-            if (decayType.representation.equals(representation)) {
-                return decayType;
+        if (reprmap.isEmpty()) {
+            for (DecayType decayType : values()) {
+                reprmap.put(decayType.representation, decayType);
             }
         }
 
-        throw new IllegalArgumentException("Invalid representation, got: " + representation);
+        DecayType decayType = reprmap.get(representation);
+        if (decayType == null) {
+            throw new IllegalArgumentException("Invalid representation, got: " + representation);
+        }
+
+        return decayType;
     }
 }
